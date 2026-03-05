106cadb47a844b1d6983f38ca3d10dde67dc8e78
SAADev08 <92945306+SAADev08@users.noreply.github.com>
2026-03-05 10:47:48 -0400

    Initial commit

---
+++ src/test/java/com/suporte/suporte_whatsapp/SuporteWhatsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suporte.suporte_whatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuporteWhatsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
